1ff8c2c13638cf9e837d645ff655e35ff242c492
yuk7 <yukx00@gmail.com>
2016-06-06 01:01:42 +0900

    install.bat :wrote new install script for windows

--- install.bat
+++ install.bat
@@ -1,4 +1,16 @@
 @echo off
 cd /d %~dp0
-mklink /h %USERPROFILE%\.gitconfig .\_git\gitconfig
-mklink /h %USERPROFILE%\.bashrc .\_bash\bashrc
+
+if not "%~1"=="" (
+	if exist _%~1\install.bat (
+		cmd /c _%~1\install.bat
+	) else if exist %~1\install.bat (
+		cmd /c %~1\install.bat
+	) else (
+		echo ERROR:The specified target ^"%~1^" does not exist.
+	)
+exit/b
+)
+
+for /f "delims=" %%i in ('dir /A:D /b ^| find "_"') do cmd /c %~dpnx0 %%i
+exit/b
