a8bae2124d38f038e128e4054314ec0abb1fb5d9
yuk7 <yukx00@gmail.com>
2016-06-07 00:59:54 +0900

    _git/install.bat :add backup function(2

--- _git/install.bat
+++ _git/install.bat
@@ -4,4 +4,15 @@
 
 @echo off
 cd /d %~dp0
+if exist %USERPROFILE%\.gitconfig.bak del /F /Q %USERPROFILE%\.gitconfig.bak
+if exist %USERPROFILE%\.gitconfig move /Y %USERPROFILE%\.gitconfig %USERPROFILE%\.gitconfig.bak
+
+
 mklink /h %USERPROFILE%\.gitconfig .\gitconfig
+
+
+if exist %USERPROFILE%\.gitconfig exit /b
+if not exist %USERPROFILE%\.gitconfig (
+echo ERROR:Installation failed.
+if exist %USERPROFILE%\.gitconfig.bak move /Y %USERPROFILE%\.gitconfig.bak %USERPROFILE%\.gitconfig
+)
