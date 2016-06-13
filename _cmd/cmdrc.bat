526779e987fa77de659e3c4e4c8805d59e8d04cf
yuk7 <yukx00@gmail.com>
2016-06-13 20:09:24 +0900

    _cmd/cmdrc.bat :add some comments

--- _cmd/cmdrc.bat
+++ _cmd/cmdrc.bat
@@ -1,9 +1,15 @@
 @echo off
 
+::macros for normal windows commands
 doskey c=cls
 doskey e=echo.
 doskey x=exit
 doskey k=taskkill
+
+::macros for Linux-like commands.(include GNU tools,etc...)
+
+
+::Additional macros
 
 
 @echo on
