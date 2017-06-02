3d91e18096669dc107556ce2f69f14b72a6449e3
yuk7 <yukx00@gmail.com>
2017-06-02 03:46:02 +0000

    _tmux/install.sh :for tmux.2.0.conf

--- _tmux/install.sh
+++ _tmux/install.sh
@@ -7,10 +7,13 @@
 DIR=$(cd $(dirname $0); pwd)
 
 mv -f ~/.tmux.conf ~/.tmux.conf.bak >/dev/null 2>&1
+mv -f ~/.tmux.2.0.conf ~/.tmux.2.0.conf.bak >/dev/null 2>&1
 
 ln -sf $DIR/tmux.conf ~/.tmux.conf
-if [ ! -e ~/.tmux.conf ]
+ln -sf $DIR/tmux.2.0.conf ~/.tmux.2.0.conf
+if [ ! -e ~/.tmux.conf -o ! -e ~/.tmux.2.0.conf ]
 then
 	echo ERROR:installation failed.
 	mv -f ~/.tmux.conf.bak ~/.tmux.conf >/dev/null 2>&1
+	mv -f ~/.tmux.2.0.conf.bak ~/.tmux.2.0.conf >/dev/null 2>&1
 fi
