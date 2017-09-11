89ea8d4d2de4032638ca8e0c6c2f7245293b353d
yuk7 <yukx00@gmail.com>
2017-09-11 02:10:08 +0000

    Add target for PowerShell

---
+++ _psh/install.bat
@@ -0,0 +1,6 @@
+:: Copyright (c) 2016 yuk7 <yukx00@gmail.com>
+:: Released under the MIT license
+:: http://opensource.org/licenses/mit-license.php
+
+@echo off
+
---
+++ _psh/install.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Copyright (c) 2016 yuk7 <yukx00@gmail.com>
+# Released under the MIT license
+# http://opensource.org/licenses/mit-license.php
+
+
+#SKIP
+#Do nothing for this target
+#This is for Windows only.
+echo "  SKIP:Do nothing for this target"
+echo "  This is for Windows only."
