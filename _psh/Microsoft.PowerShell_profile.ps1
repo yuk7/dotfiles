f1fa1e6b479c77ff9eca82810209fd5879d9e0d2
yuk7 <yukx00@gmail.com>
2017-09-11 14:12:06 +0900

    _psh/Microsoft.PowerShell_profile.ps1 :add navigation macros

--- _psh/Microsoft.PowerShell_profile.ps1
+++ _psh/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,13 @@
 function x{exit}
 function c{cls}
 function e{echo $args}
+
+#navigation macros
+function .{cd .}
+function ..{cd ..}
+function ...{cd ..\..}
+function ....{cd ..\..\..}
+function .....{cd ..\..\..\..}
+function ~{cd $env:USERPROFILE}
+function /{cd \}
+function \{cd \}
