1d4dd0132661494e7630237205a5cb62bd8dc079
yuk7 <yukx00@gmail.com>
2018-12-24 02:01:42 +0900

    _vscode/install.sh: add support for Arch's Code(OSS)

--- _vscode/install.sh
+++ _vscode/install.sh
@@ -5,16 +5,21 @@
 # http://opensource.org/licenses/mit-license.php
 
 DIR=$(cd $(dirname $0); pwd)
-DESTDIR=~/.config/Code/User
+DESTDIR="$HOME/.config/Code/User"
+DESTDIR2="$HOME/.config/Code - OSS/User"
 
-mv -f $DESTDIR/settings.json $DESTDIR/settings.json.bak >/dev/null 2>&1
+mv -f "$DESTDIR/settings.json" "$DESTDIR/settings.json.bak" >/dev/null 2>&1
+mv -f "$DESTDIR2/settings.json" "$DESTDIR2/settings.json.bak" >/dev/null 2>&1
 
-mkdir -p $DESTDIR  >/dev/null 2>&1
+mkdir -p "$DESTDIR"  >/dev/null 2>&1
+mkdir -p "$DESTDIR2"  >/dev/null 2>&1
 
-ln -sf $DIR/settings.json $DESTDIR/settings.json
+ln -sf "$DIR/settings.json" "$DESTDIR/settings.json"
+ln -sf "$DIR/settings.json" "$DESTDIR2/settings.json"
 
-if [ ! -e $DESTDIR/settings.json ]
+if [ ! -e "$DESTDIR/settings.json" ]
 then
 	echo ERROR:installation failed.
-	mv -f $DESTDIR/settings.json.bak $DESTDIR/settings.json >/dev/null 2>&1
+	mv -f "$DESTDIR/settings.json.bak" "$DESTDIR/settings.json" >/dev/null 2>&1
+	mv -f "$DESTDIR2/settings.json.bak" "$DESTDIR2/settings.json" >/dev/null 2>&1
 fi
