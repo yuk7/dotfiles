7ad21de2033b82e35cf5bfa3e6563f276d046c31
yuk7 <yukx00@gmail.com>
2019-03-19 03:39:39 +0900

    _psh: add powershell support for Linux

--- _psh/install.sh
+++ _psh/install.sh
@@ -1,12 +1,19 @@
 #! /bin/sh
 
-# Copyright (c) 2016 yuk7 <yukx00@gmail.com>
+# Copyright (c) 2019 yuk7 <yukx00@gmail.com>
 # Released under the MIT license
 # http://opensource.org/licenses/mit-license.php
 
+DIR=$(cd $(dirname $0); pwd)
+DESTDIR=~/.config/powershell
 
-#SKIP
-#Do nothing for this target
-#This is for Windows only.
-echo "  SKIP:Do nothing for this target"
-echo "  This is for Windows only."
+mkdir -p "$DESTDIR" >/dev/null 2>&1
+
+mv -f $DESTDIR/profile.ps1 $DESTDIR/profile.ps1.bak >/dev/null 2>&1
+
+ln -sf $DIR/profile.ps1 $DESTDIR/profile.ps1
+if [ ! -e ~/.config/powershell/profile.ps1 ]
+then
+	echo ERROR:installation failed.
+	mv -f $DESTDIR/profile.ps1 $DESTDIR/profile.ps1 >/dev/null 2>&1
+fi
