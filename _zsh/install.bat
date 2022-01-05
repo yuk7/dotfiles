af56b9f06437392ff0add93c90546ab44b8a8c23
yuk7 <yukx00@gmail.com>
2022-01-06 03:32:54 +0900

    _zsh: use ZDOTDIR

--- _zsh/install.bat
+++ _zsh/install.bat
@@ -3,12 +3,20 @@
 :: http://opensource.org/licenses/mit-license.php
 
 @echo off
-del /F /Q %USERPROFILE%\.zshrc.bak >NUL 2>&1
-move /Y %USERPROFILE%\.zshrc %USERPROFILE%\.zshrc.bak >NUL 2>&1
+set ZDOTDIR=%ZDOTDIR%\.config\zsh
+mkdir %ZDOTDIR% >NUL 2>&1
 
-mklink /h %USERPROFILE%\.zshrc %~dp0\zshrc
+del /F /Q %USERPROFILE%\.zshenv.bak >NUL 2>&1
+move /Y %USERPROFILE%\.zshenv %USERPROFILE%\.zshenv.bak >NUL 2>&1
+mklink /h %USERPROFILE%\.zshenv %~dp0\zshenv
 
-if not exist %USERPROFILE%\.zshrc (
+del /F /Q %ZDOTDIR%\.zshrc.bak >NUL 2>&1
+move /Y %ZDOTDIR%\.zshrc %ZDOTDIR%\.zshrc.bak >NUL 2>&1
+mklink /h %ZDOTDIR%\.zshrc %~dp0\zshrc
+
+
+if not exist %ZDOTDIR%\.zshrc (
 echo ERROR:Installation failed.
-move /Y %USERPROFILE%\.zshrc.bak %USERPROFILE%\.zshrc >NUL 2>&1
+move /Y %USERPROFILE%\.zshenv.bak %USERPROFILE%\.zshenv >NUL 2>&1
+move /Y %ZDOTDIR%\.zshrc.bak %ZDOTDIR%\.zshrc >NUL 2>&1
 )
--- _zsh/install.sh
+++ _zsh/install.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
-# Copyright (c) 2021 yuk7 <yukx00@gmail.com>
+# Copyright (c) 2021-2022 yuk7 <yukx00@gmail.com>
 # Released under the MIT license
 # http://opensource.org/licenses/mit-license.php
 
 DIR=$(cd $(dirname $0); pwd)
+if [ "$XDG_CONFIG_HOME" == "" ]
+then
+	XDG_CONFIG_HOME=~/.config
+fi
+ZSH_DOT_DIR=$XDG_CONFIG_HOME/zsh
 
-mv -f ~/.zshrc ~/.zshrc.bak >/dev/null 2>&1
+mv -f ~/.zshenv ~/.zshenv.bak >/dev/null 2>&1
+ln -sf $DIR/zshenv ~/.zshenv
 
-ln -sf $DIR/zshrc ~/.zshrc
-if [ ! -e ~/.zshrc ]
+
+mkdir -p $ZSH_DOT_DIR
+mv -f $ZSH_DOT_DIR/.zshrc ~/.zshrc.bak >/dev/null 2>&1
+ln -sf $DIR/zshrc $ZSH_DOT_DIR/.zshrc
+
+
+
+if [ ! -e $ZSH_DOT_DIR/.zshrc ]
 then
 	echo ERROR:installation failed.
-	mv -f ~/.zshrc.bak ~/.zshrc >/dev/null 2>&1
+	mv -f ~/.zshenv.bak ~/.zshenv >/dev/null 2>&1
+	mv -f $ZSH_DOT_DIR.bak $ZSH_DOT_DIR/.zshenv >/dev/null 2>&1
 fi
