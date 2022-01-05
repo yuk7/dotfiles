a793ff85a8d813f6ca1c346c0aeb25193d8b8a19
yuk7 <yukx00@gmail.com>
2022-01-06 01:53:15 +0900

    _git: use XDG_CONFIG_HOME directory

--- _git/install.bat
+++ _git/install.bat
@@ -1,18 +1,25 @@
-:: Copyright (c) 2016 yuk7 <yukx00@gmail.com>
+:: Copyright (c) 2016-2022 yuk7 <yukx00@gmail.com>
 :: Released under the MIT license
 :: http://opensource.org/licenses/mit-license.php
 
 @echo off
-del /F /Q %USERPROFILE%\.gitconfig.bak >NUL 2>&1
-del /F /Q %USERPROFILE%\.gitattributes.bak >NUL 2>&1
-move /Y %USERPROFILE%\.gitconfig %USERPROFILE%\.gitconfig.bak >NUL 2>&1
-move /Y %USERPROFILE%\.gitattributes %USERPROFILE%\.gitattributes.bak >NUL 2>&1
+set GIT_CONFIG_DIR=%USERPROFILE%\.config\git
+mkdir %GIT_CONFIG_DIR% >NUL 2>&1
 
-mklink /h %USERPROFILE%\.gitconfig %~dp0\gitconfig
-mklink /h %USERPROFILE%\.gitattributes %~dp0\gitattributes
+del /F /Q %GIT_CONFIG_DIR%\config.bak >NUL 2>&1
+del /F /Q %GIT_CONFIG_DIR%\attributes.bak >NUL 2>&1
+del /F /Q %GIT_CONFIG_DIR%\ignore.bak >NUL 2>&1
+move /Y %GIT_CONFIG_DIR%\config %GIT_CONFIG_DIR%\config.bak >NUL 2>&1
+move /Y %GIT_CONFIG_DIR%\attributes %GIT_CONFIG_DIR%\attributes.bak >NUL 2>&1
+move /Y %GIT_CONFIG_DIR%\ignore %GIT_CONFIG_DIR%\ignore.bak >NUL 2>&1
 
-if not exist %USERPROFILE%\.gitconfig (
+mklink /h %GIT_CONFIG_DIR%\config %~dp0\gitconfig
+mklink /h %GIT_CONFIG_DIR%\attributes %~dp0\gitattributes
+mklink /h %GIT_CONFIG_DIR%\ignore %~dp0\gitignore
+
+if not exist %GIT_CONFIG_DIR%\config (
 echo ERROR:Installation failed.
-move /Y %USERPROFILE%\.gitconfig.bak %USERPROFILE%\.gitconfig >NUL 2>&1
-move /Y %USERPROFILE%\.gitattributes.bak %USERPROFILE%\.gitattributes >NUL 2>&1
+move /Y %GIT_CONFIG_DIR%\config.bak %GIT_CONFIG_DIR%\config >NUL 2>&1
+move /Y %GIT_CONFIG_DIR%\attributes.bak %GIT_CONFIG_DIR%\attributes >NUL 2>&1
+move /Y %GIT_CONFIG_DIR%\ignore.bak %GIT_CONFIG_DIR%\ignore >NUL 2>&1
 )
--- _git/install.sh
+++ _git/install.sh
@@ -1,21 +1,32 @@
 #! /bin/sh
 
-# Copyright (c) 2016 yuk7 <yukx00@gmail.com>
+# Copyright (c) 2016-2022 yuk7 <yukx00@gmail.com>
 # Released under the MIT license
 # http://opensource.org/licenses/mit-license.php
 
 DIR=$(cd $(dirname $0); pwd)
 
-mv -f ~/.gitconfig ~/.gitconfig.bak >/dev/null 2>&1
-mv -f ~/.gitattributes ~/.gitattributes.bak >/dev/null 2>&1
+if [ "$XDG_CONFIG_HOME" = "" ]
+then
+	XDG_CONFIG_HOME=~/.config
+fi
 
-ln -sf $DIR/gitconfig ~/.gitconfig
-ln -sf $DIR/gitattributes ~/.gitattributes
+mkdir -p $XDG_CONFIG_HOME/git
+GIT_CONFIG_DIR=$XDG_CONFIG_HOME/git
+
+mv -f $GIT_CONFIG_DIR/config $GIT_CONFIG_DIR/config.bak >/dev/null 2>&1
+mv -f $GIT_CONFIG_DIR/attributes $GIT_CONFIG_DIR/attributes.bak >/dev/null 2>&1
+mv -f $GIT_CONFIG_DIR/ignore $GIT_CONFIG_DIR/ignore.bak >/dev/null 2>&1
+
+ln -sf $DIR/gitconfig $GIT_CONFIG_DIR/config
+ln -sf $DIR/gitattributes $GIT_CONFIG_DIR/attributes
+ln -sf $DIR/gitignore $GIT_CONFIG_DIR/ignore
 
 
-if [ ! -e ~/.gitconfig ]
+if [ ! -e $GIT_CONFIG_DIR/config ]
 then
 	echo ERROR:Installation failed.
-	mv -f ~/.gitconfig.bak ~/.gitconfig >/dev/null 2>&1
-	mv -f ~/.gitattributes.bak ~/.gitattributes >/dev/null 2>&1
+	mv -f $GIT_CONFIG_DIR/config.bak $GIT_CONFIG_DIR/config >/dev/null 2>&1
+	mv -f $GIT_CONFIG_DIR/attributes.bak $GIT_CONFIG_DIR/attributes >/dev/null 2>&1
+	mv -f $GIT_CONFIG_DIR/ignore.bak $GIT_CONFIG_DIR/ignore >/dev/null 2>&1
 fi
