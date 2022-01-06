379eed7b2fa7ed2d0ca99abe8c9530b6ed9f4988
yuk7 <yukx00@gmail.com>
2022-01-06 09:54:13 +0900

    _fontconfig,_vscode: install.sh: Use XDG_CONFIG_HOME

--- _fontconfig/install.sh
+++ _fontconfig/install.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
 
-# Copyright (c) 2018 yuk7 <yukx00@gmail.com>
+# Copyright (c) 2018-2022 yuk7 <yukx00@gmail.com>
 # Released under the MIT license
 # http://opensource.org/licenses/mit-license.php
 
 DIR=$(cd $(dirname $0); pwd)
-
-mv -f ~/.config/fontconfig ~/.config/fontconfig.bak >/dev/null 2>&1
+if [ "$XDG_CONFIG_HOME" == "" ]
+then
+	XDG_CONFIG_HOME=~/.config
+fi
+mv -f $XDG_CONFIG_HOME/fontconfig $XDG_CONFIG_HOME/fontconfig.bak >/dev/null 2>&1
 
 ln -sf $DIR ~/.config/fontconfig
 if [ ! -e ~/.bashrc ]
--- _vscode/install.sh
+++ _vscode/install.sh
@@ -1,12 +1,16 @@
 #! /bin/sh
 
-# Copyright (c) 2018 yuk7 <yukx00@gmail.com>
+# Copyright (c) 2018-2022 yuk7 <yukx00@gmail.com>
 # Released under the MIT license
 # http://opensource.org/licenses/mit-license.php
 
 DIR=$(cd $(dirname $0); pwd)
-DESTDIR="$HOME/.config/Code/User"
-DESTDIR2="$HOME/.config/Code - OSS/User"
+if [ "$XDG_CONFIG_HOME" == "" ]
+then
+	XDG_CONFIG_HOME=~/.config
+fi
+DESTDIR="$XDG_CONFIG_HOME/Code/User"
+DESTDIR2="$XDG_CONFIG_HOME/Code - OSS/User"
 
 mv -f "$DESTDIR/settings.json" "$DESTDIR/settings.json.bak" >/dev/null 2>&1
 mv -f "$DESTDIR2/settings.json" "$DESTDIR2/settings.json.bak" >/dev/null 2>&1
