020721b6776e272ad36d7da9d74a6c51fa4cacb6
yuk7 <yukx00@gmail.com>
2025-06-28 09:00:22 +0900

    _pwsh: install: add support for powershell core

--- _pwsh/install.bat
+++ _pwsh/install.bat
@@ -1,7 +1,18 @@
-:: Copyright (c) 2016 yuk7 <yukx00@gmail.com>
+:: Copyright (c) 2016-2025 yuk7 <yukx00@gmail.com>
 :: Released under the MIT license
 :: http://opensource.org/licenses/mit-license.php
 
 @echo off
 
-powershell -File %~dp0\install.ps1
+where powershell > nul 2>&1
+if %errorlevel%==0 (
+    echo =^> installing for windows powershell
+    powershell -File %~dp0\install.ps1
+)
+
+where pwsh > nul 2>&1
+if %errorlevel%==0 (
+    echo =^> installing for powershell core
+    pwsh -File %~dp0\install.ps1
+)
+echo done
