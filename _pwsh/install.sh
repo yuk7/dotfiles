ab8c72407723663784dd4e28d2e5bc48cdff6a63
yuk7 <yukx00@gmail.com>
2025-06-30 08:09:43 +0900

    _pwsh: install.sh: use install.ps1 for installation

--- _pwsh/install.sh
+++ _pwsh/install.sh
@@ -1,19 +1,13 @@
-#! /bin/sh
+#!/bin/sh
 
-# Copyright (c) 2019 yuk7 <yukx00@gmail.com>
+# Copyright (c) 2019-2025 yuk7 <yukx00@gmail.com>
 # Released under the MIT license
 # http://opensource.org/licenses/mit-license.php
 
 DIR=$(cd $(dirname $0); pwd)
-DESTDIR=~/.config/powershell
 
-mkdir -p "$DESTDIR" >/dev/null 2>&1
-
-mv -f $DESTDIR/profile.ps1 $DESTDIR/profile.ps1.bak >/dev/null 2>&1
-
-ln -sf $DIR/profile.ps1 $DESTDIR/profile.ps1
-if [ ! -e ~/.config/powershell/profile.ps1 ]
-then
-	echo ERROR:installation failed.
-	mv -f $DESTDIR/profile.ps1 $DESTDIR/profile.ps1 >/dev/null 2>&1
+if type pwsh 2>/dev/null 1>/dev/null; then
+	pwsh -File $DIR/install.ps1
+else
+	echo "  SKIP:PowerShell Core is not installed."
 fi
